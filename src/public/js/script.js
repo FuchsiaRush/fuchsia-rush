c51a5caae0a1acca50885ffa24f66d8bc69e4695
Lippmann Richard (HC/HRL-YT4) <lip1we@bosch.com>
2025-10-29 14:07:55 +0100

    feat: startgame function, joingame function fix: added area in script for all members of logic frontend

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,2 +1,54 @@
+//Richard##########################################################################################################################
+
+async function startGame(templateID) {
+    fetch(`http://path/`, {
+        header: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*",
+            "X-Requested-With": "XMLHttp"
+        },
+        method: "post",
+        body: JSON.stringify({
+            "game-code": gameID
+        })
+    })
+}
+
+function joinGame(gameID) {
+    fetch(`https://abc/${gameID}`, {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*",
+            "X-Requested-With": "XMLHttp"
+        },
+        method: "post",
+        credentials: "same-origin",
+        body: JSON.stringify({
+            "game-code": gameID
+        })
+    }
+    )
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        return response.json()
+    })
+    .then((data) => {
+        window.location.replace(`http://path/${gameID}`)
+    })
+}
+
+//##############################################################################################################################
+
+//Göki##########################################################################################################################
 
 
+
+//##############################################################################################################################
+
+//Nikola##########################################################################################################################
+
+
+
+//##############################################################################################################################
