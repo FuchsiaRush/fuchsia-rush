11d7cfabb0824807f10e7cd9b31d24938d1e7824
PatEd <patrik.edelmann@gmx.de>
2025-10-29 13:39:24 +0100

    added addQuestions function

--- src/Utils/datamodul.js
+++ src/Utils/datamodul.js
@@ -11,6 +11,30 @@ db.connect((err) => {
   if (err) {
     console.error("Fehler bei der Verbindung zu Datenbank");
   } else {
-    console.log("Datenbank verbunden!")
+    console.log("Datenbank verbunden!");
   }
 });
+
+
+//TODO: add ID generation, add Object struktur
+function addQuestion(frage) {
+  const sqlQue = "INSERT INTO fragen (FrageID, FrageSTR) VALUES (?)";
+  db.query(sqlQue, [frage], (err) => {
+    if (err) {
+      console.error("Fehler beim Einfügen", err.message);
+      return false;
+    } else {
+      return true;
+    }
+  });
+  const sqlAns =
+    "INSERT INTO antworten (AntwortID, FrageID, Antwort, T/F) VALUES (?)";
+  db.query(sqlAns, [frage], (err) => {
+    if (err) {
+      console.error("Fehler beim Einfügen", err.message);
+      return false;
+    } else {
+      return true;
+    }
+  });
+}
