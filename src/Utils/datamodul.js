b8f4376bb3dd593e3bf6aae2843729d969da31d7
Edelmann Patrik (HC/HRL-YT4) <edp3bue@bosch.com>
2025-10-29 12:44:24 +0100

    added database connection

--- src/Utils/datamodul.js
+++ src/Utils/datamodul.js
@@ -1,2 +1,16 @@
 import mysql from "mysql2";
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "fuchsia rush",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Fehler bei der Verbindung zu Datenbank");
+  } else {
+    console.log("Datenbank verbunden!")
+  }
+});
