aedb3afc0cfca0aefa73c92065bacf161a00ccff
Edelmann Patrik (HC/HRL-YT4) <edp3bue@bosch.com>
2025-10-29 12:35:10 +0100

    added databaseRouter, datamodul added dependencie mysql2 <https://github.com/FuchsiaRush/fuchsia-rush/issues/52>

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
+import databaseRouter from "./Router/databaseRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-app.use('/', express.static('src/public'));
+app.use("/", express.static("src/public"));
+app.use("/api/v1", databaseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
