0efd4bd5c48a7429b8207878f066b37e781e8103
Binici Ali (PEA6-Fe-FI) <iiw1fe@bosch.com>
2025-10-29 13:24:23 +0100

    added getAll function for tables in the database

--- src/Utils/datamodul.js
+++ src/Utils/datamodul.js
@@ -11,6 +11,25 @@ db.connect((err) => {
   if (err) {
     console.error("Fehler bei der Verbindung zu Datenbank");
   } else {
-    console.log("Datenbank verbunden!")
+    console.log("Datenbank verbunden!");
   }
 });
+
+/**
+ * Returns the values of all questions 
+ * @param tabelle 
+ * @returns all Questions and answers
+ */
+export function getAll(tabelle) {
+  return new Promise((resolve, reject) => {
+    const query = "Select * FROM ?";
+    db.query(query, [tabelle], (err, results) => {
+      if (err) {
+        console.error("Fehler bei Select:", err);
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
